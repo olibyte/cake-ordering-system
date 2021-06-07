58b65b2897286d22cbf27ab7307570b975a45a78
Oliver <ocben1@gmail.com>
2021-06-08 07:38:01 +1000

    create order

--- handler.js
+++ handler.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports.createOrder = async (event) => {
+
+  const body = JSON.parse(event.body);
+  const order = orderManager.createOrder(body);
+
+  orderManager.placeNewOrder(order);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
